8b6820c347389d7808bc3f5d3446cb8045a7a6e4
Дмитрий Ульянов <info@smartmed.info>
2023-06-16 21:36:31 +0300

    before fps

--- AvatarPlayer/Form1.cs
+++ AvatarPlayer/Form1.cs
@@ -10,8 +10,13 @@ namespace AvatarPlayer
     public partial class Form1 : Form
     {
 
-        private VideoFileSource fileSource;
-        private VideoFileReader videoReader;
+
+        private VideoFileSource fileSource1;
+        private VideoFileReader videoReader1;
+        private VideoFileSource fileSource2;
+        private VideoFileReader videoReader2;
+
+        private int currentFrameIndex = 0;
 
 
         public Form1()
@@ -24,23 +29,56 @@ namespace AvatarPlayer
 
         }
 
-        private void buttonStart_Click(object sender, EventArgs e)
-        {
-        }
-
-
-
+        
         private void videoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
-            // Обновляем изображение на форме при получении кадра видео
-            pictureBoxVideo.Image = (Bitmap)eventArgs.Frame.Clone();
+            // Получаем текущее количество кадров в секунду каждого из видео
+            double fps1 = (double)videoReader1.FrameRate;
+            double fps2 = (double)videoReader2.FrameRate;
+
+            // Переключаем на следующий кадр
+            currentFrameIndex++;
+
+            // Определяем откуда выводить следующий кадр
+            VideoFileReader currentVideoReader;
+            if (IsDivisible(currentFrameIndex, 2))
+            {
+                currentVideoReader = videoReader2;
+            }
+            else
+            {
+                currentVideoReader = videoReader1;
+            }
+
+            // Получаем кадр из текущего видео и обновляем изображение на форме
+            Bitmap currentFrame = currentVideoReader.ReadVideoFrame();
+            pictureBoxVideo.Image = currentFrame;
+
+            // Освобождаем память от старого кадра
+            if (currentFrameIndex > 1)
+            {
+                //currentVideoReader.CloseFrame();
+            }
+
+            // При достижении конца видео останавливаем его и перезапускаем показ видео с начала
+            //if (currentVideoReader.IsNewFrame())
+            //{
+            //    currentVideoReader.CloseFrame();
+            //    currentVideoReader.Seek(0, SeekOrigin.Begin);
+            //}
         }
 
         private void buttonStop_Click(object sender, EventArgs e)
         {
-            // Останавливаем видео
-            fileSource.Stop();
-            videoReader.Close();
+            // Останавливаем видео и освобождаем память
+            fileSource1.Stop();
+            videoReader1.Close();
+            videoReader1.Dispose();
+            fileSource2.Stop();
+            videoReader2.Close();
+            videoReader2.Dispose();
+            currentFrameIndex = 0;
+            pictureBoxVideo.Image = null;
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)
@@ -50,24 +88,42 @@ namespace AvatarPlayer
 
         private void button2_Click(object sender, EventArgs e)
         {
-            // Выбираем файл видео через диалог открытия файла
-            //OpenFileDialog openFileDialog = new OpenFileDialog();
-            //openFileDialog.Filter = "AVI files (*.avi)|*.avi|MP4 files (*.mp4)|*.mp4";
-            //if (openFileDialog.ShowDialog() == DialogResult.OK)
-            //{
-            string videoFile = @"d:\Left.wmv";
-                // Создаем источник видео из выбранного файла
-                fileSource = new VideoFileSource(videoFile);
-                videoReader = new VideoFileReader();
-                videoReader.Open(videoFile);
+            // Выбираем файлы видео через диалог открытия файла
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Создаем источники видео из выбранных файлов
+                fileSource1 = new VideoFileSource(openFileDialog.FileName);
+                videoReader1 = new VideoFileReader();
+                videoReader1.Open(openFileDialog.FileName);
+
+                // Переключаем на второй файл и создаем источник видео
+                openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    fileSource2 = new VideoFileSource(openFileDialog.FileName);
+                    videoReader2 = new VideoFileReader();
+                    videoReader2.Open(openFileDialog.FileName);
+                }
+                else
+                {
+                    buttonStop_Click(sender, e);
+                    return;
+                }
 
                 // Устанавливаем обработчик события, который будет вызываться при получении нового кадра
-                fileSource.NewFrame += new NewFrameEventHandler(videoSource_NewFrame);
+                fileSource1.NewFrame += new NewFrameEventHandler(videoSource_NewFrame);
 
                 // Показываем видео
-                fileSource.Start();
-            //}
+                fileSource1.Start();
+            }
+        }
 
+        public bool IsDivisible(int x, int n)
+        {
+            return (x % n) == 0;
         }
 
         private void Form1_Load_1(object sender, EventArgs e)
