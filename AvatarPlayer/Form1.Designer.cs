ddaa647739a9731abac32ee2101b3352c947b51a
Дмитрий Ульянов <info@smartmed.info>
2023-06-16 05:44:13 +0300

    player

---
+++ AvatarPlayer/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace AvatarPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBoxVideo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVideo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(185, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBoxVideo
+            // 
+            this.pictureBoxVideo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxVideo.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxVideo.Name = "pictureBoxVideo";
+            this.pictureBoxVideo.Size = new System.Drawing.Size(284, 261);
+            this.pictureBoxVideo.TabIndex = 1;
+            this.pictureBoxVideo.TabStop = false;
+            this.pictureBoxVideo.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBoxVideo);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVideo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBoxVideo;
+    }
+}
+
---
+++ AvatarPlayer/Form1.cs
@@ -0,0 +1,78 @@
+﻿using Accord.Video;
+using Accord.Video.FFMPEG;
+using System.Drawing;
+using System.Windows.Forms;
+using System;
+
+
+namespace AvatarPlayer
+{
+    public partial class Form1 : Form
+    {
+
+        private VideoFileSource fileSource;
+        private VideoFileReader videoReader;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+        }
+
+
+
+        private void videoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            // Обновляем изображение на форме при получении кадра видео
+            pictureBoxVideo.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            // Останавливаем видео
+            fileSource.Stop();
+            videoReader.Close();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Выбираем файл видео через диалог открытия файла
+            //OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.Filter = "AVI files (*.avi)|*.avi|MP4 files (*.mp4)|*.mp4";
+            //if (openFileDialog.ShowDialog() == DialogResult.OK)
+            //{
+            string videoFile = @"d:\Left.wmv";
+                // Создаем источник видео из выбранного файла
+                fileSource = new VideoFileSource(videoFile);
+                videoReader = new VideoFileReader();
+                videoReader.Open(videoFile);
+
+                // Устанавливаем обработчик события, который будет вызываться при получении нового кадра
+                fileSource.NewFrame += new NewFrameEventHandler(videoSource_NewFrame);
+
+                // Показываем видео
+                fileSource.Start();
+            //}
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
